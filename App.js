1796e01fa983f818836f4cefb9b3496c4fcb6a3b
potechi57 <92413757+potechi57@users.noreply.github.com>
2021-11-08 15:38:19 +0900

    Create App.js

---
+++ App.js
@@ -0,0 +1,11 @@
+import { Weather } from "./components/index"
+
+function App() {
+    return (
+      <div className="mx-36 flex justify-center flex-wrap items-center">
+        <Weather city_name="Tokyo"/>
+      </div>
+    )
+  
+  
+ export defalut App;
